ce1aca289196a2a1a67fe4edae56648dd5bafe64
Apoh <Edenra08@gmail.com>
2022-03-26 01:48:14 +0000

    Assigment complete

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+
+
+var customerName = "bob";
+var bestCustomer;
+const myCustomer = "important";
+
+
+function upperCaseCustomerName(){
+
+    
+   return customerName = customerName.toUpperCase();
+    //return customerName.toUpperCase();
+
+}
+
+
+function setBestCustomer(){
+return bestCustomer = "not bob"
+
+}
+
+
+function overwriteBestCustomer(){
+
+    return bestCustomer = "maybe bob"
+}
+
+
+function changeLeastFavoriteCustomer(){
+return myCustomer = "Miss";
+
+}
